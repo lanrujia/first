a0a362d8f256e5849d9f6ad2c3402eba932fbd17
admin <admin@qq.com>
2020-11-18 20:28:04 +0800

    新的项目第一次提交

--- demo1/src/com/lrj/test/Test1.java
+++ demo1/src/com/lrj/test/Test1.java
@@ -2,6 +2,6 @@ package com.lrj.test;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("hello git!!!啊啊啊啊");
+        System.out.println("hello git!!!");
     }
 }
