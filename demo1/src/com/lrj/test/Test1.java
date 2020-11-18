e6ebdd054757701405f5de719e4e072163275a6f
lanrujia <64236716+lanrujia@users.noreply.github.com>
2020-11-18 20:33:24 +0800

    改代码啦
    我要改代码！！！

--- demo1/src/com/lrj/test/Test1.java
+++ demo1/src/com/lrj/test/Test1.java
@@ -4,4 +4,9 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
     }
+    
+    //啦啦啦改代码啦
+    public void fun1(){
+        System.out.println("hello");
+    }
 }
